168d2a7446e7db81f90ef5e7a6e37d2d4cfe401f
Bowen <bowenbhwu@gmail.com>
2022-11-13 01:11:07 +0800

    Add: Specialized stream data handler for Amazon

---
+++ handlers/amazon_stream_data_handler.py
@@ -0,0 +1,144 @@
+import os
+import re
+import random
+import pickle
+import numpy as np
+
+from .stream_data_handler import StreamDataHandler
+
+class AmazonStreamDataHandler(StreamDataHandler):
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+
+    def load(self, **kwargs):
+        super().load(**kwargs)
+
+    """
+    Note: All stream retrieved using this method has indices starting from 0
+    """
+    def load_stream(self, t=None):
+        if t is not None:
+            self.t = t
+
+        stream_base_path = os.path.join('./data', self.data_name, 'streams', str(self.t))
+        stream_statistics_path = os.path.join(stream_base_path, 'statistics')
+        stream_adj_list_path = os.path.join(stream_base_path, 'adj_list.pkl')
+        stream_features_path = os.path.join(stream_base_path, 'features')
+        stream_train_nodes_path = os.path.join(stream_base_path, 'train_nodes')
+        stream_val_nodes_path = os.path.join(stream_base_path, 'val_nodes')
+        stream_labels_path = os.path.join(stream_base_path, 'labels')
+        with open(stream_statistics_path, 'r') as f:
+            stats = f.read().split('\n')
+        self.stream_stats = {}
+        for stat in stats:
+            splitted = stat.split('=')
+            if len(splitted) == 2:
+                key = splitted[0]
+                val = splitted[1]
+                # Convert to int when possible
+                try:
+                    val = int(val)
+                except:
+                    pass
+                self.stream_stats[key] = val
+        
+        # Read labels
+        self.stream_labels = np.loadtxt(stream_labels_path, dtype = np.int64, delimiter=',')[:, 1]
+
+        # Obtain train-test split
+        self.train_nodes = np.loadtxt(stream_train_nodes_path, dtype = np.int64, delimiter=',') - self.stream_stats['lo']
+        self.val_nodes = np.loadtxt(stream_val_nodes_path, dtype = np.int64, delimiter=',') - self.stream_stats['lo']
+
+        # Load the features
+        self.features = np.loadtxt(stream_features_path, delimiter=',')
+        # self.X_train = features[self.train_nodes, :]
+        # self.X_val = features[self.val_nodes, :]
+        self.y_train = self.stream_labels[self.train_nodes]
+        self.y_val = self.stream_labels[self.val_nodes]
+
+        # Load adjacency list
+        lo = self.stream_stats['lo']
+        with open(stream_adj_list_path, 'rb') as fp:
+            adj_list = pickle.load(fp)
+            out_adj_list = {}
+            for node, adj in adj_list.items():
+                out_adj_list[node-lo] = (np.array(adj_list[node])-lo).tolist()
+            self.adj_list = out_adj_list
+
+        # print(self.X_train.shape, self.X_val.shape, self.y_train.shape, self.y_val.shape)
+
+    """
+    Load relations for a given stream. 
+    The loaded relations are stored in self.relations[relation]
+    * Note: Only the specified stream is loaded.
+    * Note: All stream retrieved using this method has indices starting from 0
+    Must call load_stream first.
+    """
+    def load_stream_relations(self, t=None, relations=['upu', 'usv', 'uvu']):
+        if t is not None:
+            self.t = t
+        if not hasattr(self, 'stream_stats'):
+            raise Exception('need to invoke load_stream() first')
+
+        lo = self.stream_stats['lo']
+
+        self.relations = {}
+        stream_base_path = os.path.join('./data', self.data_name, 'streams', str(self.t))
+        
+        for relation in relations:
+            relation_path = os.path.join(stream_base_path, f'{relation}_adj_list.pkl')
+            with open(relation_path, 'rb') as fp:
+                adj_list = pickle.load(fp)
+                out_adj_list = {}
+                for node, adj in adj_list.items():
+                    out_adj_list[node-lo] = (np.array(adj_list[node])-lo).tolist()
+                self.relations[relation] = out_adj_list
+
+    """
+    Sample a graph with random-walk-with-restrart
+    Arguments:
+        root: The root node. Default: a random node
+        pr: The probability of restarting in any step
+        max_steps: The maximum number of steps to walk
+    """
+    def sameple_graph(self, root=None, pr=0.2, max_steps=100):
+        if not hasattr(self, 'stream_stats'):
+            raise Exception('need to invoke load_stream() first')
+
+        if root is None:
+            stream_range = self.stream_stats['hi'] - self.stream_stats['lo'] + 1
+            root_node = random.randint(0, stream_range-1)
+        else:
+            root_node = root
+        curr_node = root_node
+
+        edges = set()
+        nodes = set()
+
+        # Add the root node
+        nodes.add(root_node)
+
+        for i in range(max_steps):
+            # Determine whether to restart
+            if random.random() < pr:
+                curr_node = root_node
+                continue
+            curr_node_adj_list_len = len(self.adj_list[curr_node])
+            if curr_node_adj_list_len > 0:
+                next_node = self.adj_list[curr_node][random.randint(0, curr_node_adj_list_len-1)]
+                # Add the edge i->j (i < j)
+                if next_node != curr_node:
+                    # In case the adjacency list contains self-loop
+                    edges.add((curr_node, next_node) if curr_node < next_node else (next_node, curr_node))
+                    nodes.add(next_node)
+                curr_node = next_node
+        
+        adj_list = {}
+        for node in nodes:
+            adj_list[node] = []
+        for node1, node2 in edges:
+            adj_list[node1].append(node2)
+            adj_list[node2].append(node1)
+        return list(nodes), adj_list
+
--- handlers/stream_data_handler.py
+++ handlers/stream_data_handler.py
@@ -1,5 +1,6 @@
 import os
 import sys
+import pickle
 import numpy as np 
 import logging
 import random
@@ -9,42 +10,49 @@ from .data_handler import DataHandler
 
 class StreamDataHandler(DataHandler):
 
-    def __init__(self):
+    def __init__(self, data_name=None, t=None, max_detect_size=None):
         super(StreamDataHandler, self).__init__()
-
-    def load(self, data_name, t, lo_t=None):
         self.data_name = data_name
         self.t = t
+        self.max_detect_size = max_detect_size
+
+    def load(self, data_name=None, t=None, max_detect_size=None):
+        if data_name is not None:
+            self.data_name = data_name
+        if t is not None:
+            self.t = t
+        if max_detect_size is not None:
+            self.max_detect_size = max_detect_size
 
         # Load features
-        features_file_name = os.path.join('./data', data_name, 'features')
+        features_file_name = os.path.join('./data', self.data_name, 'features')
         self.features = np.loadtxt(features_file_name, delimiter=',')
 
         # Load labels
-        labels_file_name = os.path.join('./data', data_name, 'labels')
+        labels_file_name = os.path.join('./data', self.data_name, 'labels')
         labels = np.loadtxt(labels_file_name, dtype = np.int64, delimiter=',')
 
         # Load train / valid nodes
-        train_file_name = os.path.join('./data', data_name, 'train_nodes')
+        train_file_name = os.path.join('./data', self.data_name, 'train_nodes')
         self.train_all_nodes_list = np.loadtxt(train_file_name, dtype = np.int64, delimiter=',')
-        valid_file_name = os.path.join('./data', data_name, 'val_nodes')
+        valid_file_name = os.path.join('./data', self.data_name, 'val_nodes')
         self.valid_all_nodes_list = np.loadtxt(valid_file_name, dtype = np.int64, delimiter=',')
 
         # Load graph
-        stream_dir_name = os.path.join('./data', data_name, 'streams')
+        stream_dir_name = os.path.join('./data', self.data_name, 'streams')
         # self.nodes = set()
         self.train_cha_nodes_list, self.train_old_nodes_list = set(), set()
         self.valid_cha_nodes_list, self.valid_old_nodes_list = set(), set()
         self.adj_lists = defaultdict(set)
         
-        begin_time = 0 if lo_t is None else t - lo_t
-        end_time = t
-        for tt in range(0, len(os.listdir(os.path.join('./data', data_name, 'streams')))):
-            stream_edges_file_name = os.path.join(stream_dir_name, str(tt), 'edges')
+        begin_time = 0 if self.max_detect_size is None else max(self.t - self.max_detect_size, 0)
+        end_time = self.t
+        for tt in range(0, len(os.listdir(os.path.join('./data', self.data_name, 'streams')))):
             stream_train_node_file_name = os.path.join(stream_dir_name, str(tt), 'train_nodes')
             stream_val_node_file_name = os.path.join(stream_dir_name, str(tt), 'val_nodes')
 
             if tt <= end_time and tt >= begin_time:
+                stream_adj_list_file_name = os.path.join(stream_dir_name, str(tt), 'adj_list.pkl')
                 if os.path.exists(stream_train_node_file_name):
                     train_nodes = np.loadtxt(stream_train_node_file_name, dtype = np.int64, delimiter=',').tolist()
                     val_nodes = np.loadtxt(stream_val_node_file_name, dtype = np.int64, delimiter=',').tolist()
@@ -54,14 +62,14 @@ class StreamDataHandler(DataHandler):
                     elif tt == self.t:
                         self.train_cha_nodes_list.update(train_nodes)
                         self.valid_cha_nodes_list.update(val_nodes)
-                    with open(stream_edges_file_name) as fp:
-                        for i, line in enumerate(fp):
-                            info = line.strip().split(',')
-                            node1, node2 = int(info[0]), int(info[1])
-                            
-                            self.adj_lists[node1].add(node2)
-                            self.adj_lists[node2].add(node1)
+                    with open(stream_adj_list_file_name, 'rb') as fp:
+                        new_adj_list = pickle.load(fp)
+                    for node, node_adj_list in new_adj_list.items():
+                        # Undirected information handled by preprocessing script
+                        self.adj_lists[node].update(node_adj_list)
                 else:
+                    # Legacy
+                    stream_edges_file_name = os.path.join(stream_dir_name, str(tt), 'edges')
                     with open(stream_edges_file_name) as fp:
                         for i, line in enumerate(fp):
                             info = line.strip().split(',')
